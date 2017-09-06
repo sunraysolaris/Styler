6d5bf69ce2f1dd28eb868059cd824cfa57d640e6
gzhghamadze <gzhghamadze@GZHGHAMADZE>
2017-09-06 16:28:05 +0400

    Footer, Header and Slideshow are now partial views

--- Styler/Controllers/HomeController.cs
+++ Styler/Controllers/HomeController.cs
@@ -9,9 +9,22 @@ namespace Styler.Controllers
 {
     public class HomeController : BaseController
     {
-        public ActionResult Index()
+        public ActionResult Index(int categoryId = 0, string keyword = null) // TODO: maxPrice and Minprice to add in search filter
         {
-            var products = DbContext.Products.ToList();
+            var productQuery = DbContext.Products.AsQueryable();
+
+            if (categoryId > 0)
+            {
+                var category = DbContext.Categories.SingleOrDefault(c => c.CategoryID == categoryId);
+                productQuery = productQuery.Where(p => p.ProductCategories.Contains(category));
+            }
+
+            var products = productQuery.Where(p => p.Description.Contains(keyword)
+                                                     || p.ProductName.Contains(keyword)
+                                                     || p.ProductNameEng.Contains(keyword)
+                                                     || p.ProductNameRus.Contains(keyword)
+                                                     || keyword == null)
+                                                    .ToList(); // filter by keyword
             return View(products);
         }
 
